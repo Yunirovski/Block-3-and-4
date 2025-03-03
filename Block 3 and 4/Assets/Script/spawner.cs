b1e83cca72fbc05061fca1da718298a22345294d
yunchangzhang <46707148+yunchangzhang@users.noreply.github.com>
2025-03-03 15:02:20 +0100

    Enemy mover and generator

--- Block 3 and 4/Assets/Script/spawner.cs
+++ Block 3 and 4/Assets/Script/spawner.cs
@@ -1,14 +1,17 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class spawner : MonoBehaviour
 {
     [SerializeField] private GameObject obstacle;
+    [SerializeField] private int amount = 100;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < amount; i++)
         {
-            Instantiate(obstacle, Random.insideUnitCircle * 10, Quaternion.identity);
+            Vector2 rnd = Random.insideUnitCircle;
+            Vector3 pos = new Vector3(rnd.x, 0, rnd.y);
+            Instantiate(obstacle, pos * 10, Quaternion.identity);
         }
     }
 
