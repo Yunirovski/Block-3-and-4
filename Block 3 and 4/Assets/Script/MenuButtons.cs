c25c144dd88ff29c067429856bf70b425f4117a9
JstSilver <nt.stanemir@st.hanze.nl>
2025-05-22 16:30:28 +0200

    switch menu

--- Block 3 and 4/Assets/Script/MenuButtons.cs
+++ Block 3 and 4/Assets/Script/MenuButtons.cs
@@ -1,6 +1,22 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+using System.Collections.Generic;
 
 public class MenuButtons
 {
-    
+    void SwitchScene(string SceneName){
+
+        SceneManager.LoadScene(SceneName);
+    }
+
+    void SwitchMenu(string NextMenu){
+
+
+    }
+
+    void Quit()
+    {
+        Application.Quit();
+    }
 }
